5bb3b14ff24472a7cf5961637eef92f6218df9fa
sraju728@gmail.com <sraju728@gmail.com>
2018-11-10 14:00:30 +0530

    Importing required packages and Libraries

---
+++ EmotionRecognitionUsingFacialExpressionswithCNNs.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sivaprasadrb
+#
+# Created:     10/11/2018
+# Copyright:   (c) sivaprasadrb 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+from __future__ import print_function
+import keras
+import os
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras import optimizers
