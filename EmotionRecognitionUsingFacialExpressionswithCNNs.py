1131b0922f7103142736636babc67b0b807404b5
sraju728@gmail.com <sraju728@gmail.com>
2018-11-10 14:33:59 +0530

    Creating the CNN model with 3 convolutional layers, 3 pooling layers and 3 fully connected layers

--- EmotionRecognitionUsingFacialExpressionswithCNNs.py
+++ EmotionRecognitionUsingFacialExpressionswithCNNs.py
@@ -89,3 +89,23 @@ print(type(train_labels))
 
 print('Original label 0 : ', train_labels[0])
 print('After conversion to categorical ( one-hot ) : ', train_labels_one_hot[0])
+
+def createModel():
+    model = Sequential()
+    model.add(Conv2D(10, (3, 3), padding='same', activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(10, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Conv2D(10, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nClasses, activation='softmax'))
+
+    return model
